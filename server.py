3bb716f7915689c93123fb79d24c9a1fb7b6f108
Pedro Marques <pedro.marques@negocioanimal.com.br>
2023-08-23 21:14:20 -0300

    remove unused code at server.py

--- server.py
+++ server.py
@@ -72,34 +72,16 @@ async def create_upload_file(file: UploadFile):
     db.close()
 
     # return with status 200
-    return {"message": "File successfully uploaded", "file": new_file}
-
-    # # Fetch the bulk download of results from the parameter "download_url"
-    # print("Fetch the results from: ", get_generated_files_from_project_id(project_id))
-
-    # # Download the files
-    # get_pdb_file_from_project_id(project_id)
-
-    # # extract the pdb file from the gz file
-    # extract_pdb_file_from_gz_file("tmp/01.pdb.gz")
-
-    # # convert the pdb file to a mol2 file
-    # convert_pdb_to_mol2()
-
-    # os.remove("tmp/" + filename)
-
-    # return {"message": "File successfully converted to mol2"}
+    return {"message": "File successfully uploaded", "file": new_file} 
 
 
 @app.get("/status/{project_id}")
 async def get_job_status(project_id: str):
-    # Update the status from the server
+    # Get the status from the server
         response = requests.get(
             f"https://swissmodel.expasy.org/project/{project_id}/models/summary/",
             headers={"Authorization": f"Token {swissmodel_token}"})
 
-        # Update the status
-        
         return response.json()
 
 
